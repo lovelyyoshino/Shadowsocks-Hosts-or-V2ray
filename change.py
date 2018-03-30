ef3261f4fe396456c8eb284f41fef645c01eebd7
lovelyyoshino <mpl9725440@gmail.com>
2018-03-30 17:43:03 +0800

    加入ipv4

--- change.py
+++ change.py
@@ -3,13 +3,18 @@ import os
 
 f = open('C:\Windows\System32\drivers\etc\hosts','w')
 
-checked = ccbox(msg='请选择',title='host修改器',choices=('翻墙','恢复'))
+checked = ccbox(msg='请选择是否翻墙',title='host修改器',image="google.gif",choices=('翻墙','恢复'))
 if checked:
-    fq = open('hosts',encoding='UTF-8') .read()
-    f.write(fq)
+	checked1 = ccbox(msg='请选择网络类型',title='网络类型',image="google.gif",choices=('IPv4','IPv6'))
+	if checked1:
+		fq = open('hostsv4',encoding='UTF-8') .read()
+		f.write(fq)
+	else:
+		fq = open('hostsv6',encoding='UTF-8') .read()
+		f.write(fq)
 else:
-    hf = open('oldhost').read()
-    f.write(hf)
+	hf = open('oldhost').read()
+	f.write(hf)
 
 cmd = os.popen('ipconfig /flushdns');
 info = cmd.readlines()
